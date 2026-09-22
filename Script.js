3042e397ec49ce2c4654d5da30c1641c1c68ba02
fatianuri16 <fatianuri@student.upi.edu>
2026-09-22 20:19:56 +0700

    Update website files

---
+++ Script.js
@@ -0,0 +1,13 @@
+// =====================================================
+// TUGAS MANDIRI
+// SISTEM POIN & KEANGGOTAAN MEMBER KEDAI KOPI
+// =====================================================
+
+
+// =====================================================
+// AKTIVITAS 1 : SETUP BERKAS & INTEGRASI JAVASCRIPT
+// =====================================================
+
+console.log("=== SISTEM POIN MEMBER KEDAI KOPI ===");
+
+console.log("Javascript Terhubung!");
